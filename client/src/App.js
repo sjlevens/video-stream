e5cd8449af31297dbde74d5827dd459c07d54609
slevens <sjlevens@gmail.com>
2021-06-07 10:39:21 +1000

    .

---
+++ client/src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg'
+import './App.css'
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <video style={{ height: '400px', width: '600px' }} controls autoPlay>
+          <source src="videoproxy/02398203598" type="video/mp4" />
+        </video>
+      </header>
+    </div>
+  )
+}
+
+export default App
